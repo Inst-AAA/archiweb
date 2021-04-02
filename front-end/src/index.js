b5817279a2ffaafbeb434d539777f69d48192835
Yichen Mo <amomorning@gmail.com>
2021-04-02 21:13:13 +0800

    Select bbox buildings

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -23,14 +23,81 @@ let control = {
     bbox.forEach((p) => {
       range.push(mousePos(map.project(p)))
     });
-    let feature = map.queryRenderedFeatures(range, {
+    let features = map.queryRenderedFeatures(range, {
       layers: ['building']
     });
-    for (let i = 0; i < feature.length; ++i) {
-      highlightBuilding(feature[i], 'h' + i.toString());
+  
+    // add one layer and source
+    let feature = JSON.parse(JSON.stringify(features[0]));
+    feature.toJSON = features[0].toJSON;
+    feature.geometry.coordinates = [];
+    for (let i = 0; i < features.length; ++i) {
+      feature.geometry.coordinates.push(features[i].geometry.coordinates);
     }
-    
-  }
+    feature.geometry.type = 'MultiPolygon';
+    highlightBuilding(feature, '-aabb');
+  
+    // add multiple layer and source
+    // for (let i = 0; i < features.length; ++i) {
+    //   highlightBuilding(features[i], 'h' + i.toString());
+    // }
+  },
+  show3D: false
+}
+
+
+
+function initGUI() {
+  gui = new dat.GUI({autoPlace: false});
+  
+  util = gui.addFolder('Utils');
+  util.add(control, 'randomCenter').name('center');
+  util.add(control, 'getAABB').name('getAABB');
+  util.add(control, 'show3D').onChange(() => {
+    if (control.show3D) {
+      
+      map.addLayer(
+        {
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 16.6,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+            
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              17,
+              0,
+              17.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              17,
+              0,
+              17.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.96
+          },
+        },
+      );
+      
+    } else {
+      map.removeLayer('3d-buildings')
+    }
+  })
+  util.open();
+  
+  const container = document.getElementById('gui-container');
+  container.appendChild(gui.domElement);
 }
 
 function mousePos(e) {
@@ -41,20 +108,6 @@ function mousePos(e) {
     e.y - rect.top - canvas.clientTop
   )
 }
-
-
-function initGUI() {
-  gui = new dat.GUI({autoPlace: false});
-  
-  util = gui.addFolder('Utils');
-  util.add(control, 'randomCenter').name('center');
-  util.add(control, 'getAABB').name('getAABB');
-  util.open();
-  
-  const container = document.getElementById('gui-container');
-  container.appendChild(gui.domElement);
-}
-
 
 function highlightBuilding(feature, id = '') {
   if (typeof map.getLayer('building-highlighted' + id) !== "undefined" &&
@@ -93,7 +146,9 @@ function main() {
     container: 'map', // container ID
     style: 'mapbox://styles/amomorning/ckmzydbvf0aik18obwp77yn5g', // style URL
     center: [16.373, 48.208], // starting position [lng, lat]
-    zoom: 17 // starting zoom
+    minZoom: 11,
+    zoom: 15.1,
+    antialias: true
   });
   
   map.addControl(new mapboxgl.FullscreenControl(), 'bottom-left');
@@ -102,20 +157,22 @@ function main() {
   map.on('load', () => {
     
     map.on('mousemove', function (e) {
-        let feature = map.queryRenderedFeatures(e.point, {layers: ['building']})[0];
-        if (feature === undefined) {
-          if (typeof map.getLayer('building-highlighted') !== "undefined") {
-            map.removeLayer('building-highlighted')
+        if (!control.show3D) {
+          let feature = map.queryRenderedFeatures(e.point, {layers: ['building']})[0];
+          if (feature === undefined) {
+            if (typeof map.getLayer('building-highlighted') !== "undefined") {
+              map.removeLayer('building-highlighted')
+            }
+            if (typeof map.getSource('building-highlighted') !== "undefined") {
+              map.removeSource('building-highlighted')
+            }
+          } else {
+        
+            highlightBuilding(feature);
+        
           }
-          if (typeof map.getSource('building-highlighted') !== "undefined") {
-            map.removeSource('building-highlighted')
-          }
-        } else {
-  
-          highlightBuilding(feature);
-  
+      
         }
-    
       }
     );
   
